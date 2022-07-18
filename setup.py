d5ece102fa0838e6673279d00d76ac6dcbf54e01
Irem Altan <iremaltan@Irems-MacBook-Pro.local>
2022-07-17 22:52:50 -0400

    updated __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='patchyAnalysisTools',
+    version='0.0.1',
+    description='simple tools to analyze output of patchy particle MC sims',
+    url='https://github.com/sodiumnitrate/patchyAnalysisTools.git',
+    author='Irem Altan',
+    author_email='irem.altan@yale.edu',
+    license='',
+    packages=['patchyAnalysisTools'],
+    zip_safe=False
+)
